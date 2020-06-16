032fb2468578f29bf6b804886299bdfb54179940
Renan Biegelmeyer <renan.biegel@gmail.com>
2020-06-16 14:37:05 -0300

    improved to parse a local file

--- website_downloader/wb_downloader.py
+++ website_downloader/wb_downloader.py
@@ -6,16 +6,15 @@ from bs4 import BeautifulSoup
 
 
 def init_argparse() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(
-        usage="%(prog)s [OPTIONS] [URL] [OUTPUT]...",
-        description="Downloads the given website.",
-    )
-    parser.add_argument(
-        "-v", "--version", action="version",
-        version=f"{parser.prog} version 1.0.0"
-    )
+    parser = argparse.ArgumentParser(prog='wb_downloader',
+                                     usage='%(prog)s [OPTIONS] [OUTPUT]...',
+                                     description='Downloads the given website.')
 
-    parser.add_argument('url', type=str, help='website to download')
+    parser.add_argument('-v', '--version', action='version', version=f'{parser.prog} version 1.0.0')
+
+    parser.add_argument('-i', '--input', action='store', type=str, required=True,
+                        help='website to download, or input file to process.')
+    # parser.add_argument('url', type=str, help='website to download')
     parser.add_argument('output', type=str, help='output of the website downloaded')
     return parser
 
@@ -46,14 +45,25 @@ def _is_fb_pixel(image):
     return False
 
 
-def _extract_images_to_download(page, url):
+def _open_saved_page(file_path):
+    if not os.path.exists(f'{file_path}/index.html'):
+        raise ValidationException('File does not exist')
+
+    with open(f'{file_path}/index.html', 'r') as fh:
+        lines = fh.read()
+    return lines
+
+
+def _extract_images_to_download(page, url=None):
     raw_imgs = page.find_all('img')
     images = []
     for raw in raw_imgs:
         if _is_fb_pixel(raw.attrs['src']):
             continue
 
-        joined_url = parse.urljoin(url, raw.attrs['src'])
+        joined_url = raw.attrs['src']
+        if url:
+            joined_url = parse.urljoin(url, raw.attrs['src'])
 
         if joined_url not in images:
             images.append(joined_url)
@@ -64,11 +74,11 @@ def _extract_images_to_download(page, url):
 def run() -> None:
     parser = init_argparse()
     args = parser.parse_args()
-    _create_output_dir(args.output)
-    page = download_and_save_page(args.url, args.output)
+    # _create_output_dir(args.output)
+    # page = download_and_save_page(args.url, args.output)
+    page = _open_saved_page(args.output)
     soup = BeautifulSoup(page, 'html.parser')
-    _extract_images_to_download(soup, args.url)
-
+    print(_extract_images_to_download(soup))
 
 
 if __name__ == "__main__":
